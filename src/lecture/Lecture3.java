6fe4816b845dfd1734ca92e6c523a9458c33f7e8
cjy324 <juy32400@gmail.com>
2020-10-14 13:28:13 +0900

    20.10.14 수업내용

--- src/lecture/Lecture3.java
+++ src/lecture/Lecture3.java
@@ -6,96 +6,46 @@ public class Lecture3 {
 	
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
-
-		// 1번 게시물 저장소
-		int article1__id = 0;
-		String article1__title = "";
-		String article1__body = "";
-
-		// 2번 게시물 저장소
-		int article2__id = 0;
-		String article2__title = "";
-		String article2__body = "";
+		testArticle a0 = new testArticle();
+		a0.id = 1;
+		a0.title = "제목 1";
+		a0.body = "내용 1";
+		System.out.printf("id : %d, title : %s, body : %s \n", a0.id, a0.title, a0.body);
 
 		
-		int lastArticleId = 0;
+		testArticle a1 = new testArticle();
+		a1.id = 2;
+		a1.title = "제목 2";
+		a1.body = "내용 2";
+		System.out.printf("id : %d, title : %s, body : %s \n", a1.id, a1.title, a1.body);
 		
 		
-		while (true) {
-			System.out.printf("명령어) ");
-			String command = scanner.nextLine();
+		
 
-			if (command.startsWith("article detail ")) {
-				int inputedId = Integer.parseInt(command.split(" ")[2]);
+		testArticle a3;         //a3이라는 하나의 리모컨 객체를 생성하는 것
+		a3 = new testArticle(); //a3이라는 리모컨에 기능을 부여하는 것
 
-				System.out.println("== 게시물 상세 ==");
-
-				if (inputedId == 1) {
-					if (article1__id == 0) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
-						continue;
-					}
-
-					System.out.printf("번호 : %s\n", article1__id);
-					System.out.printf("제목 : %s\n", article1__title);
-					System.out.printf("내용 : %s\n", article1__body);
-				} else if (inputedId == 2) {
-					if (article2__id == 0) {
-						System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
-						continue;
-					}
-
-					System.out.printf("번호 : %s\n", article2__id);
-					System.out.printf("제목 : %s\n", article2__title);
-					System.out.printf("내용 : %s\n", article2__body);
-				} else {
-					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
-				}
-
-			}
-
-			else if (command.equals("article list")) {
-				System.out.println("== 게시물 리스트 ==");
-
-				if (lastArticleId == 0) {
-					System.out.println("게시물이 없습니다.");
-					continue;
-				}
-				System.out.println("번호 / 제목");
-				if (lastArticleId >= 1) {
-					System.out.printf("%d / %s\n", article1__id, article1__title);
-				}
-				if (lastArticleId >= 2) {
-					System.out.printf("%d / %s\n", article2__id, article2__title);
-				}
-			} else if (command.equals("article add")) {
-				System.out.println("== 게시물 등록 ==");
-				int id = lastArticleId + 1;
-				String title;
-				String body;
-				System.out.printf("제목 : ");
-				title = scanner.nextLine();
-				System.out.printf("내용 : ");
-				body = scanner.nextLine();
-				if (id == 1) {
-					article1__id = id;
-					article1__title = title;
-					article1__body = body;
-				} else if (id == 2) {
-					article2__id = id;
-					article2__title = title;
-					article2__body = body;
-				}
-				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
-
-				lastArticleId = id;
-			} else if (command.equals("system exit")) {
-				System.out.println("== 프로그램 종료 ==");
-				break;
-			}
-
-		}
-		scanner.close();
+		//즉, a3이 id, title, body를 모두 담아내는 객체가 되는 것이 아닌(변수에는 오직 하나의 값만 담을 수 있기에..)
+		//id, title, body를 원격으로 조종할 수 있는 리모컨 기능을 갖고 있는 객체가 되는 것
+		//이런 개념을 레퍼런스 변수라 함
+		a3.id = 10;
+		a3.title = "제목 1";
+		a3.body = "내용 1";
+		testArticle a4 = a3;
+		a4.id = 20;
+		System.out.println(a4.id);
+		
+		
+		
+		
+		
+		
+		
+		
 	}
+}
+class testArticle{
+	public int id;
+	public String title;
+	public String body;
 }
--- src/practice/Practice3.java
+++ src/practice/Practice3.java
@@ -1,9 +1,11 @@
-package com.sbs.example.easytextboard;
+package practice;
 
 import java.util.Scanner;
 
-public class Main {
+public class Practice3 {
+
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
 
 		// 1번 게시물 저장소
@@ -15,6 +17,7 @@ public class Main {
 		int lastArticleId = 0;
 
 		while (true) {
+
 			System.out.printf("명령어) ");
 			String command = scanner.nextLine();
 
@@ -45,35 +48,33 @@ public class Main {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
 				}
 
-			} else if (command.equals("article list")) {
+			}
+			
+			
+
+			else if (command.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 
 				if (lastArticleId == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
-
 				System.out.println("번호 / 제목");
-
 				if (lastArticleId >= 1) {
 					System.out.printf("%d / %s\n", article1.id, article1.title);
 				}
-
 				if (lastArticleId >= 2) {
 					System.out.printf("%d / %s\n", article2.id, article2.title);
 				}
 			} else if (command.equals("article add")) {
 				System.out.println("== 게시물 등록 ==");
-
 				int id = lastArticleId + 1;
 				String title;
 				String body;
-
 				System.out.printf("제목 : ");
 				title = scanner.nextLine();
 				System.out.printf("내용 : ");
 				body = scanner.nextLine();
-
 				if (id == 1) {
 					article1.id = id;
 					article1.title = title;
@@ -83,11 +84,8 @@ public class Main {
 					article2.title = title;
 					article2.body = body;
 				}
-
 				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 
-				// 가장 마지막 게시물 번호를 갱신한다.
-				// 왜냐하면 방금 새 게시물이 생성되었기 때문.
 				lastArticleId = id;
 			} else if (command.equals("system exit")) {
 				System.out.println("== 프로그램 종료 ==");
@@ -95,7 +93,11 @@ public class Main {
 			}
 
 		}
-
 		scanner.close();
 	}
+}
+class Article {
+	int id = 0;
+	String title = "";
+	String body = "";
 }
