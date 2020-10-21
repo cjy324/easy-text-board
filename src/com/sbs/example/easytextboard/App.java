753a396fa7492ab6851e564fe70c0e50a03431c0
cjy324 <juy32400@gmail.com>
2020-10-21 11:17:18 +0900

    20.10.21 수업내용(게시물 삭제 추가)

--- src/com/sbs/example/easytextboard/App.java
+++ src/com/sbs/example/easytextboard/App.java
@@ -3,95 +3,137 @@ package com.sbs.example.easytextboard;
 import java.util.Scanner;
 
 public class App {
-	Article[] articles = new Article[10]; // 10칸의 articles라는 Article배열을 만들고
+	Article[] articles = new Article[3];
+	int lastArticleId = 0;
+	int articlesSize = 0;
 
-	public Article getArticle(int id) {
-		if (id > articles.length) {
+	int articlesSize() {
+		return articlesSize;
+	}
+
+	Article getArticle(int id) {
+		int index = getIndexById(id);
+		
+		if ( index == -1 ) {
 			return null;
 		}
-
-		return articles[id - 1];
-	} 
+		
+		return articles[index];
+	}
 
 	public void run() {
-		for (int i = 0; i < articles.length; i++) { // articles라는 공간에 게시물 채워 넣기
-			articles[i] = new Article();
-		}
-
 		Scanner sc = new Scanner(System.in);
 
-		int lastArticleId = 0;
+		int maxArticlesCount = articles.length;
 
 		while (true) {
+
 			System.out.printf("명령어) ");
 			String command = sc.nextLine();
 
-			if (command.equals("article add")) {
-				System.out.println("== 새 게시물 등록 ==");
+			if (command.equals("system exit")) {
+				System.out.println("== 프로그램 종료 ==");
+				break;
+			} else if (command.equals("article add")) {
+				System.out.println("== 게시물 등록 ==");
+
+				if (articlesSize() >= maxArticlesCount) {
+					System.out.println("더 이상 생성할 수 없습니다.");
+					continue;
+				}
 
 				int id = lastArticleId + 1;
 				String title;
 				String body;
 
 				lastArticleId = id;
-				System.out.printf("제목 입력 : ");
-				title = sc.nextLine();
-				System.out.printf("제목 : %s\n", title);
-				System.out.printf("내용 입력 : ");
-				body = sc.nextLine();
-				System.out.printf("내용 : %s\n", body);
 
-				Article article = getArticle(id);
+				System.out.printf("제목 : ");
+				title = sc.nextLine();
+				System.out.printf("내용 : ");
+				body = sc.nextLine();
+
+				Article article = new Article();
 
 				article.id = id;
 				article.title = title;
 				article.body = body;
 
-			}
+				System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 
-			else if (command.equals("article list")) {
-				if (lastArticleId == 0) {
-					System.out.println("== 게시물이 존재하지 않습니다. ==");
+				articles[articlesSize] = article;
+
+				articlesSize++;
+			} else if (command.equals("article list")) {
+				System.out.println("== 게시물 리스트 ==");
+
+				if (articlesSize() == 0) {
+					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				System.out.println("== 게시물 리스트 ==");
+
 				System.out.println("번호 / 제목");
 
-				for (int i = 1; i <= lastArticleId; i++) {
-					Article article = getArticle(i);
+				for (int i = 0; i < articlesSize(); i++) {
+					Article article = articles[i];
 
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
-			}
-
-			else if (command.startsWith("article detail ")) {
+			} else if (command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 상세 ==");
 
-				Article selectedArticle = getArticle(inputedId);
+				Article article = getArticle(inputedId);
 
-				if (selectedArticle == null || selectedArticle.id == 0) {
-					System.out.printf("== %d 번 게시물이 존재하지 않습니다. ==\n", inputedId);
+				if (article == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
 					continue;
 				}
 
-				System.out.println("== 게시물 상세 ==");
-				System.out.printf("번호: %d\n", selectedArticle.id);
-				System.out.printf("제목 : %s\n", selectedArticle.title);
-				System.out.printf("내용 : %s\n", selectedArticle.body);
-			}
+				System.out.printf("번호 : %d\n", article.id);
+				System.out.printf("제목 : %s\n", article.title);
+				System.out.printf("내용 : %s\n", article.body);
+			} else if (command.startsWith("article delete ")) {
+				int inputedId = Integer.parseInt(command.split(" ")[2]);
+				System.out.println("== 게시물 삭제 ==");
 
-			else if (command.equals("system exit")) {
-				System.out.println("== 프로그램 종료 ==");
-				break;
-			} else {
-				System.out.println("== 해당 명령어가 없습니다. ==");
-				continue;
-			}
+				Article article = getArticle(inputedId);
 
+				if (article == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", inputedId);
+					continue;
+				}
+
+				remove(inputedId);
+
+				System.out.printf("%d번 게시물이 삭제되었습니다.\n", inputedId);
+			}
 		}
 
 		sc.close();
-
 	}
 
+	private void remove(int id) {
+		int index = getIndexById(id);
+
+		if (index == -1) {
+			return;
+		}
+
+		for (int i = index + 1; i < articlesSize(); i++) {
+			articles[i - 1] = articles[i];
+		}
+
+		articlesSize--;
+	}
+
+	private int getIndexById(int id) {
+		for (int i = 0; i < articlesSize(); i++) {
+			if (articles[i].id == id) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
 }
